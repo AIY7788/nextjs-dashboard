8806aaf5b272c31101fadf4298d4fc3d520373ba
AIY7788 <aiykhounnakhon@gmail.com>
2026-03-25 09:22:36 +0700

    improve form accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,9 +59,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
   } catch (error) {
     console.error(error);
-    // return {
-    //   message: "Database Error: Failed to Create Invoice.",
-    // };
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
   }
 
   revalidatePath("/dashboard/invoices");
@@ -73,13 +73,22 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 // ...
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice.",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
@@ -90,9 +99,9 @@ export async function updateInvoice(id: string, formData: FormData) {
     `;
   } catch (error) {
     console.error(error);
-    // return {
-    //   message: "Database Error: Failed to Update Invoice.",
-    // };
+    return {
+      message: "Database Error: Failed to Update Invoice.",
+    };
   }
 
   revalidatePath("/dashboard/invoices");
@@ -100,8 +109,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
-  
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
